0e61993ea061c4a36ca3a08e31fb3484bad17425
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-08-11 19:31:15 +0700

    archiving

---
+++ README.md
@@ -0,0 +1,4 @@
+# P2P Energy Trading - ARS IoT Hackaton 2020 
+
+IoT Mobile App where you can keep track of your Energy generated from BPV Cell and trade your Energy with other households. 
+We Used Antares IoT Platform to Manage all the data that's coming from all sensors. App Prototype is built with MIT App Inventor 2 
---
+++ src/appinventor/ai_davidfauzi29/HackatonLombaARSfinal/Loading.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"207","Source":"Form","Properties":{"$Name":"Loading","$Type":"Form","$Version":"27","AccentColor":"&H00FFFFFF","AppName":"HackatonLombaARSfinal","BackgroundColor":"&HFF000000","Title":"Loading","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"Button1","$Type":"Button","$Version":"6","BackgroundColor":"&HFF000000","Height":"-1100","Width":"-1100","Uuid":"1562511341"}]}}
+|#
